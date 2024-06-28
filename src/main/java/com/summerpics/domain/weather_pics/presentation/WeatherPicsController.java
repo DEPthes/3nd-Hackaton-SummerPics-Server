9d81c820af594259ca6b2f7c14545836354269cc
HuniHuns <i690367@gmail.com>
2024-06-29 05:31:24 +0900

    [feat] 좋아요, 순위 배열 기능

--- src/main/java/com/summerpics/domain/weather_pics/presentation/WeatherPicsController.java
+++ src/main/java/com/summerpics/domain/weather_pics/presentation/WeatherPicsController.java
@@ -1,30 +1,20 @@
 package com.summerpics.domain.weather_pics.presentation;
 
 import com.summerpics.domain.weather_pics.application.WeatherPicsService;
-import com.summerpics.domain.weather_pics.domain.WeatherPics;
-import com.summerpics.domain.weather_pics.dto.ThumsRequestDTO;
-import com.summerpics.domain.weather_pics.dto.ThumsResponseDTO;
+import com.summerpics.domain.weather_pics.dto.request.ThumsRequestDTO;
+import com.summerpics.domain.weather_pics.dto.response.ThumsResponseDTO;
 import com.summerpics.domain.weather_pics.dto.WeatherPicsRankDTO;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import com.summerpics.domain.weather_pics.dto.request.WeatherInfoReq;
-import com.summerpics.domain.weather_pics.dto.response.WeatherPicRes;
-import lombok.RequiredArgsConstructor;
-import org.json.simple.parser.ParseException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -47,11 +37,6 @@ public class WeatherPicsController {
     @GetMapping("/rank")
     public List<WeatherPicsRankDTO> getWeatherPicsRank() {
         return weatherPicsService.getWeatherPicsRank();
-
-    @PostMapping("/")
-    public ResponseEntity<WeatherPicRes> getWeatherPic(@RequestBody WeatherInfoReq request) throws IOException, ParseException {
-        WeatherPicRes weatherPicRes = weatherPicsService.getWeatherPic(request);
-        return ResponseEntity.ok(weatherPicRes);
     }
 }
 
